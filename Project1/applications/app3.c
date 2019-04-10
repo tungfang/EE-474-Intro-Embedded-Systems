057ebfa433046de1f97af4cfe88638fd7d564a27
tungfang <tungfang@uw.edu>
2019-04-10 16:05:40 -0700

    fix bug

--- Project1/applications/app3.c
+++ Project1/applications/app3.c
@@ -3,41 +3,42 @@
 // To use time library of C
 #include <time.h>
 
-void delay(int number_of_seconds);
-
-// Driver code to test above function
-int main(void)
-{
-    int count = 0;
-    // note spec is unclear what offset B D should flash in relation to A and C
-	while (1) {
-        // print all when reach both flash rate's least common multiple 
-        if (count % 6 == 0) 
-            printf("\rA B C D");
-        else if (count % 6 == 1 || count % 6 ==5)
-            printf("\r       "); 
-        else if (count % 6 == 2 || count % 6 == 4)
-            printf("\rA   C  "); 
-        else
-            printf("\r  B   D");
-        
-        count++;
-        fflush(stdout);
-        delay(1);
-	}
-
-	return 0;
-}
 
 void delay(int number_of_seconds)
 {
     // Converting time into milli_seconds
     int milli_seconds = 1000 * number_of_seconds;
 
-    // Storing start time
+    // Stroing start time
     clock_t start_time = clock();
 
     // looping till required time is not acheived
     while (clock() < start_time + milli_seconds)
         ;
+}
+// Driver code to test above function
+int main(void)
+{
+    // note spec is unclear what offset B D should flash in relation to A and C
+	while (1) {
+        // Print each of these strings, overwriting the previous
+        // line and persist for 1000 millis
+        printf ("\rA   C  ");
+        fflush(stdout);
+        delay(1);
+
+        printf ("\r       ");
+        fflush(stdout);
+        delay(1);
+
+        printf ("\rA B C D");
+        fflush(stdout);
+        delay(1);
+
+        printf ("\r  B   D");
+        fflush(stdout);
+        delay(1);
+	}
+
+	return 0;
 }
